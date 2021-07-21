7f1c76ec09247dd721efef3f17e9d5321fb29d03
KhamdullaevUz <elbekh75@gmail.com>
2021-07-21 08:44:33 +0500

    adding files

---
+++ src/Method.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @author Elbek Khamdullaev
+ * @description php post method other site with curl
+ * @license free and open source
+ * @repository https://github.com/KhamdullaevUz/post-method-curl
+ */
+
+namespace Method;
+
+class Post{
+
+	public function createPost($url, $datas = []){
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST,1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS,$datas);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$output = curl_exec($ch);
+		curl_close($ch);
+
+		return $output;
+	}
+}
